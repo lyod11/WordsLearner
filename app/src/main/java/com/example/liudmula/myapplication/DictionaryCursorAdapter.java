ac4b53651b1095b9975c9b36e2d963bd6f5da46a
LiudmylaPatenko <lubilla03@gmail.com>
2016-10-12 14:41:21 +0300

    Create custom cursor adapter

---
+++ app/src/main/java/com/example/liudmula/myapplication/DictionaryCursorAdapter.java
@@ -0,0 +1,45 @@
+package com.example.liudmula.myapplication;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by liudmula on 12.10.16.
+ */
+
+public class DictionaryCursorAdapter extends CursorAdapter {
+
+
+    public DictionaryCursorAdapter(Context context, Cursor cursor) {
+        super(context, cursor, 0);
+    }
+
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.dict_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        TextView textView = (TextView) view.findViewById(R.id.tvWord);
+        String string = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.WORD));
+        textView.setText(string);
+
+        textView = (TextView) view.findViewById(R.id.tvDescription);
+        string = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.DESC));
+        textView.setText(string);
+
+        textView = (TextView) view.findViewById(R.id.tvProgress);
+        string = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.PROGRESS));
+        textView.setText(string);
+
+
+    }
+}
