8b04623914d94211cf9e12e4c2561042aa9e8e21
LiudmylaPatenko <lubilla03@gmail.com>
2016-10-12 15:02:44 +0300

    It should set the custom adapter, but i'm not sure that it's work :)

--- app/src/main/java/com/example/liudmula/myapplication/DictionaryFragment.java
+++ app/src/main/java/com/example/liudmula/myapplication/DictionaryFragment.java
@@ -1,17 +1,34 @@
 package com.example.liudmula.myapplication;
 
 
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DictionaryFragment extends Fragment {
+
+    private DBManager dbManager;
+
+    private DictionaryCursorAdapter adapter;
+
+    private ListView listView;
+
+    private View view;
+
+//    final String[] from = new String[] { DatabaseHelper._ID,    its unused
+//        DatabaseHelper.WORD, DatabaseHelper.DESC, DatabaseHelper.PROGRESS };
+
 
 
     @Override
@@ -21,4 +38,20 @@ public class DictionaryFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_dictionary, container, false);
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        dbManager = new DBManager(this.getContext());
+        dbManager.open();
+        //make the cursor global and private?
+        Cursor cursor = dbManager.fetch();
+
+        listView = (ListView) view.findViewById(R.id.lvDict);
+
+       // listView.setEmptyView(); що за єрєсь?
+
+        adapter = new DictionaryCursorAdapter(this.getContext(), cursor);
+        listView.setAdapter(adapter);
+    }
 }
