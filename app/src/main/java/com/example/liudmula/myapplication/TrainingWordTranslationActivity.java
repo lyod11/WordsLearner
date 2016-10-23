7428456c355d1ea9d81dd9436cc06e8ce9eb4282
LiudmylaPatenko <lubilla03@gmail.com>
2016-10-23 18:52:04 +0300

    Create xml to word-translation training

---
+++ app/src/main/java/com/example/liudmula/myapplication/TrainingWordTranslationActivity.java
@@ -0,0 +1,13 @@
+package com.example.liudmula.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TrainingWordTranslationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_training_word_translation);
+    }
+}
