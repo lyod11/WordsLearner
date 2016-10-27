c60a9d93d42b94a1f44ca506c45437aeb80ac5ce
LiudmylaPatenko <lubilla03@gmail.com>
2016-10-27 19:23:14 +0300

    fix t-w repeat

--- app/src/main/java/com/example/liudmula/myapplication/TrainingActivity.java
+++ app/src/main/java/com/example/liudmula/myapplication/TrainingActivity.java
@@ -10,25 +10,23 @@ import android.widget.TextView;
 
 
 public class TrainingActivity extends Activity {
-    Button btnTrans1, btnTrans2, btnTrans3, btnTrans4, btnTrans5, btnTrans6;
-    TextView tvWord;
-    final Handler handler = new Handler();
-    int delayTime = 2000;
-    int correctAnswers = 0;
-    Integer qnumber = 3;
-    Training training;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training);
         int trainingFragmentIndex = getIntent().getIntExtra("training", -1);
-        boolean tratingType = getIntent().getBooleanExtra("type", true);
+        boolean trainingType = getIntent().getBooleanExtra("type", true);
         Fragment fragment = null;
         Class fragmentClass = null;
+        Bundle bundle = new Bundle();
         if(trainingFragmentIndex == 0){
             fragmentClass = WordTranslationFragment.class;
+            bundle.putBoolean("type", getIntent().getBooleanExtra("type", true));
+        }
+        if(trainingFragmentIndex == 1){
+            fragmentClass = WordTranslationFragment.class;
+            bundle.putBoolean("type", getIntent().getBooleanExtra("type", false));
         }
 
         try {
@@ -38,8 +36,8 @@ public class TrainingActivity extends Activity {
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
-        Bundle bundle = new Bundle();
-        bundle.putBoolean("type", getIntent().getBooleanExtra("type", true));
+
+
         FragmentManager fragmentManager = getFragmentManager();
         fragment.setArguments(bundle);
         fragmentManager.beginTransaction().replace(R.id.training_frame, fragment).commit();
