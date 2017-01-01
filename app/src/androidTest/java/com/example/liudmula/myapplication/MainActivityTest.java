3336d846be70bf92ae12fa5f963d4745f068542f
LiudmylaPatenko <lubilla03@gmail.com>
2017-01-01 17:24:47 +0200

    Work is done, but still a lot of things to do

--- app/src/androidTest/java/com/example/liudmula/myapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/example/liudmula/myapplication/MainActivityTest.java
@@ -9,26 +9,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.action.ViewActions.*;
-import static android.support.test.espresso.assertion.ViewAssertions.*;
-import static android.support.test.espresso.matcher.ViewMatchers.*;
-
-import com.example.liudmula.myapplication.R;
-
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -40,79 +37,79 @@ public class MainActivityTest {
     @Test
     public void mainActivityTest() {
         ViewInteraction appCompatImageButton = onView(
-allOf(withContentDescription("Open navigation drawer"),
-withParent(withId(R.id.toolbar)),
-isDisplayed()));
+                allOf(withContentDescription("Open navigation drawer"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
         appCompatImageButton.perform(click());
-        
-        ViewInteraction appCompatImageButton2 = onView(
-allOf(withContentDescription("Open navigation drawer"),
-withParent(withId(R.id.toolbar)),
-isDisplayed()));
-        appCompatImageButton2.perform(click());
-        
-        ViewInteraction appCompatCheckedTextView = onView(
-allOf(withId(R.id.design_menu_item_text), withText("Словники"), isDisplayed()));
-        appCompatCheckedTextView.perform(click());
-        
-        ViewInteraction appCompatEditText = onView(
-allOf(withId(R.id.etTypeWrd), isDisplayed()));
-        appCompatEditText.perform(click());
-        
-        ViewInteraction floatingActionButton = onView(
-allOf(withId(R.id.fab), isDisplayed()));
-        floatingActionButton.perform(click());
-        
-        ViewInteraction appCompatEditText2 = onView(
-allOf(withId(R.id.etAddWord), isDisplayed()));
-        appCompatEditText2.perform(replaceText("h"), closeSoftKeyboard());
-        
-        ViewInteraction appCompatEditText3 = onView(
-allOf(withId(R.id.etAddDesc), isDisplayed()));
-        appCompatEditText3.perform(replaceText("rrr"), closeSoftKeyboard());
-        
-        ViewInteraction appCompatButton = onView(
-allOf(withId(R.id.btn_Add_word), withText("ДОДАТИ"), isDisplayed()));
-        appCompatButton.perform(click());
-        
-        ViewInteraction appCompatImageButton3 = onView(
-allOf(withContentDescription("Open navigation drawer"),
-withParent(withId(R.id.toolbar)),
-isDisplayed()));
-        appCompatImageButton3.perform(click());
-        
-        ViewInteraction appCompatCheckedTextView2 = onView(
-allOf(withId(R.id.design_menu_item_text), withText("Словники"), isDisplayed()));
-        appCompatCheckedTextView2.perform(click());
-        
-        ViewInteraction gridLayout = onView(
-allOf(childAtPosition(
-withId(R.id.lvDict),
-0),
-isDisplayed()));
-        gridLayout.perform(click());
-        
-        ViewInteraction appCompatButton2 = onView(
-allOf(withId(R.id.btn_Delete_word), withText("Видалити"), isDisplayed()));
-        appCompatButton2.perform(click());
-        
-        ViewInteraction appCompatImageButton4 = onView(
-allOf(withContentDescription("Open navigation drawer"),
-withParent(withId(R.id.toolbar)),
-isDisplayed()));
-        appCompatImageButton4.perform(click());
-        
-        ViewInteraction appCompatCheckedTextView3 = onView(
-allOf(withId(R.id.design_menu_item_text), withText("Тренування"), isDisplayed()));
-        appCompatCheckedTextView3.perform(click());
-        
-        ViewInteraction appCompatButton3 = onView(
-allOf(withId(R.id.btn_w_t), withText("Слово-переклад"), isDisplayed()));
-        appCompatButton3.perform(click());
-        
-        }
 
-        private static Matcher<View> childAtPosition(
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        ViewInteraction appCompatCheckedTextView = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("Словники"), isDisplayed()));
+        appCompatCheckedTextView.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.etTypeWrd), isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab), isDisplayed()));
+        floatingActionButton.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.etAddWord), isDisplayed()));
+        appCompatEditText2.perform(replaceText("h"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.etAddDesc), isDisplayed()));
+        appCompatEditText3.perform(replaceText("rrr"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_Add_word), withText("ДОДАТИ"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatImageButton3 = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatImageButton3.perform(click());
+
+        ViewInteraction appCompatCheckedTextView2 = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("Словники"), isDisplayed()));
+        appCompatCheckedTextView2.perform(click());
+
+        ViewInteraction gridLayout = onView(
+                allOf(childAtPosition(
+                        withId(R.id.lvDict),
+                        0),
+                        isDisplayed()));
+        gridLayout.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btn_Delete_word), withText("Видалити"), isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction appCompatImageButton4 = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        withParent(withId(R.id.toolbar)),
+                        isDisplayed()));
+        appCompatImageButton4.perform(click());
+
+        ViewInteraction appCompatCheckedTextView3 = onView(
+                allOf(withId(R.id.design_menu_item_text), withText("Тренування"), isDisplayed()));
+        appCompatCheckedTextView3.perform(click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.btn_w_t), withText("Слово-переклад"), isDisplayed()));
+        appCompatButton3.perform(click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
 
         return new TypeSafeMatcher<View>() {
@@ -126,8 +123,8 @@ allOf(withId(R.id.btn_w_t), withText("Слово-переклад"), isDisplayed
             public boolean matchesSafely(View view) {
                 ViewParent parent = view.getParent();
                 return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup)parent).getChildAt(position));
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
     }
-    }
+}
